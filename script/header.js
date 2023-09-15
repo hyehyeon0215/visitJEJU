61c40f850be407272a956875d426633447a3751c
hyehyeon0215 <qwert0880@naver.com>
2023-09-16 06:31:34 +0900

    탭 메뉴 구현

--- script/header.js
+++ script/header.js
@@ -1,6 +1,10 @@
 const header_gnb = document.querySelectorAll('.header-gnb > li');
 const languageIcon = document.querySelector('.languageIcon');
-const languageList = document.querySelector(".language");
+const languageList = document.querySelector('.language');
+const menuIcon =document.querySelector('.menuIcon');
+const tab_menu = document.querySelector('.tab-menu');
+const tab_close = document.querySelector('.tab-close');
+const tab_li = document.querySelectorAll('.tab-gnb > li');
 
 header_gnb.forEach((el)=>{
     el.addEventListener(("mouseover"), (e) => {
@@ -25,4 +29,30 @@ languageIcon.addEventListener(("click"), (e) => {
     } else {
         languageList.classList.add('on');
     }
+})
+
+menuIcon.addEventListener(("click"), (e)=>{
+    e.preventDefault();
+    tab_menu.classList.add('active');
+})
+
+tab_close.addEventListener(("click"), (e)=>{
+    e.preventDefault();
+    tab_menu.classList.remove('active');
+
+    tab_li.forEach((el)=>{
+        el.classList.remove("gray");
+    })
+})
+
+tab_li.forEach((el)=>{
+    el.addEventListener("mouseover", (e)=> {
+        e.preventDefault();
+        tab_li.forEach((el)=>{
+            el.classList.remove("on");
+            el.classList.add("gray");
+        })
+        el.classList.remove("gray");
+        el.classList.add("on");
+    })
 })
